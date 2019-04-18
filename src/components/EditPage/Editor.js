c132ef5a5686eeaf4fcfa74059cd2e8489e83236
Madison Hershkovitz <mlhersh6@gmail.com>
2019-04-17 22:33:04 -0500

    editor implemented and functional

---
+++ src/components/EditPage/Editor.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+
+//creating the text editor and making functionality
+class Editor extends Component {
+    state = {
+        text: "",
+    }
+
+
+
+    handleChange = (html) => {
+        this.setState({ editorHtml: html });
+    }
+
+    modules = {
+        toolbar: [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            ['link', 'image'],
+            ['clean']
+        ],
+    }
+
+    formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+    ]
+
+    render() {
+        return (
+            <div>
+
+                <ReactQuill theme="snow"
+                    modules={this.modules}
+                    formats={this.formats}>
+                </ReactQuill>
+            </div>
+        )
+    }
+}
+
+const mapReduxStateToProps = reduxState => ({
+    reduxState
+});
+
+export default connect(mapReduxStateToProps)(Editor);
