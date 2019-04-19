d3a15ba2e752c13fb121d821793f8bb56fd2936d
JuliaAugustSugarman <jules.august@gmail.com>
2019-04-19 11:05:17 -0500

    added sweet alerts

--- src/components/UploadPage/upload.js
+++ src/components/UploadPage/upload.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import swal from 'sweetalert';
 import '../App/App.css';
 
 
@@ -16,13 +17,34 @@ class UploadPage extends Component {
         // upload file to GC Storage
     }
     
+    handleCancelButton = () => {
+        console.log('in SweetAlert Cancel Button');
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    swal("Poof! Your imaginary file has been deleted!", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Your imaginary file is safe!");
+                }
+            });
+
+        
+    }
 
     render() {
         return (
             <>
                 <button onClick={this.handleChooseFileButton}>Choose File</button>
                 <div>
-                    <button>Cancel</button>
+                    <button onClick={this.handleCancelButton}>Cancel</button>
                     <button onClick={this.handleUploadButton}>Upload</button>
                 </div>
             </>
