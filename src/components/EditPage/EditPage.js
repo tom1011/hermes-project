08c87ee52c552f73d9769c9f70010b0edabcd699
Madison Hershkovitz <mlhersh6@gmail.com>
2019-04-20 17:10:26 -0500

    added functionality to cancel buttons

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -20,7 +20,7 @@ class EditPage extends Component {
         console.log('in SweetAlert Cancel Button');
         swal({
             title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this imaginary file!",
+            text: "Careful, you will lose all progress and information forever!",
             icon: "warning",
             buttons: true,
             dangerMode: true,
@@ -30,6 +30,7 @@ class EditPage extends Component {
                     swal("Poof! Your imaginary file has been deleted!", {
                         icon: "success",
                     });
+                    this.props.history.push('/platforms');
                 } else {
                     swal("Your imaginary file is safe!");
                 }
